468f94954664670b1b1cb9d056ab6eb2c98b7924
Hoang Trung <trungdoublelift@gmail.com>
2021-12-25 20:20:32 +0700

    .

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -8,7 +8,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Teacher } from 'src/app/models/Teacher.model';
 import { Parent } from 'src/app/models/Parent.model';
 import { Class } from 'src/app/models/Class.model';
-import {} from '../../shared/shared.module'
+import { } from '../../shared/shared.module'
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
@@ -111,7 +111,7 @@ export class StudentComponent implements OnInit {
   }
   //Support Function
   async fillUpdateStudentForm(Id: any) {
-    await this.getStudentDetail(Id);
+    this.getStudentDetail(Id);
     this.getAllTeacher();
     this.getAllParent();
     this.getAllClass();
@@ -125,6 +125,7 @@ export class StudentComponent implements OnInit {
         Teacher: this.selectedStudent.Teacher[0]._id,
         Class: this.selectedStudent.Class[0]._id,
       })
+
     }, 250)
 
   }
---
+++ src/app/models/Subject.model.ts
@@ -0,0 +1,9 @@
+
+export class Subject {
+  constructor(
+    public _id?: string | null,
+    public Title?: string | null,
+    public Description?: string | null,
+
+  ) { }
+}
